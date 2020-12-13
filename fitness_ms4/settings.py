66cd7fa626c1e5b0e2cd24023745c4807beb477c
rimantascode <r.preiks@gmail.com>
2020-12-13 09:17:25 +0000

    added the secret key in the Environment Variables

--- fitness_ms4/settings.py
+++ fitness_ms4/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9xdh7tx2)hf6_%4#aaba+t!78rm0e)&ef4%^kvfbwt2e=$$-rl'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
