041d5429df4e7671a1b12dc0a3bb83669b2cbbfd
rimantascode <r.preiks@gmail.com>
2020-12-13 17:05:27 +0000

    the last time I created the models in wrong app home instead of products, so fixed that and also added templates including the views

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+        'sku',
+        'image_url'
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
