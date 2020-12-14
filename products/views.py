c48e9035eedce77c8695aa7617eb863e316a98c2
rimantascode <r.preiks@gmail.com>
2020-12-14 13:27:32 +0000

    sorting the products by price and rating

--- products/views.py
+++ products/views.py
@@ -9,9 +9,25 @@ def all_products(request):
 
     query = None
     categories = None
+    sort = None
+    direction = None
 
     products = Product.objects.all()
-
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+       
     if 'category' in request.GET:
         categories = request.GET['category'].split(',')
         products = products.filter(category__name__in=categories)
@@ -28,11 +44,13 @@ def all_products(request):
             description__icontains=query)
 
         products = products.filter(queries)
-      
+
+    current_sorting = f'{sort}_{direction}'  
     context = {
         'products': products,
         'search_term': query,
-        'current_category': categories
+        'current_category': categories,
+        'current_sorting': current_sorting
     }
     return render(request, 'products/products.html', context)
 
