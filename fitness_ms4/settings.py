d3cb91bb57215f036269a37871dac18fa1650c2f
rimantascode <r.preiks@gmail.com>
2020-12-15 12:56:55 +0000

    cart views, urls ,and templates

--- fitness_ms4/settings.py
+++ fitness_ms4/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
-    'products'
+    'products',
+    'cart'
 ]
 
 MIDDLEWARE = [
@@ -73,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents'
             ],
         },
     },
@@ -152,3 +154,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD=50
+STANDARD_DELIVERY_PERCENTAGE=5
+
