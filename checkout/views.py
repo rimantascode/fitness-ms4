c9b87dad6a388e6e06041c7bb78b6c149e5a69c9
rimantascode <r.preiks@gmail.com>
2020-12-21 13:03:35 +0000

    added basic payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-
+from django.conf import settings
 from .forms import OrderForm
 
+from cart.contexts import cart_contents
+
+import stripe
+import json
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-
+    current_bag = cart_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key':'pk_test_51I0jWJA3o7Dh28QGfwFw9XqG9jpTEAm5o7TTgSWzrmqhel4gToXE8LymGb9GhSoRHjrmufpJeR3e1nbinXjV61dZ00Vn7K8bza',
-        'client_secret':'client secret test',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
 
 
     }
