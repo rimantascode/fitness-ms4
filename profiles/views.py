0d4bc1bbcc8e3154cc258e9d304755f5b6e78aab
rimantascode <r.preiks@gmail.com>
2020-12-23 12:57:58 +0000

    added security

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -11,7 +11,7 @@ def updated(request):
 
     return render(request, 'profiles/updated_profile.html')
 
-
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
